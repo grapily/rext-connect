bdc0300bb2f4771b599b4812836e12c166729002
Luca Lanziani <luca.lanziani@gmail.com>
2011-12-13 17:57:18 +0100

    cleaned test and changed module to document

--- test/server.js
+++ test/server.js
@@ -1,40 +1,16 @@
 var connect = require('../node_modules/connect/')
   , rext_connect = require('../lib/rext-connect')
+  , Rext = require('../node_modules/rext')
   , app = connect()
   , port = 3000
-  , prefix = "myapp"
+  , prefix = "echo"
   ;
 
-function echo(options, callback) {
-  callback(null, JSON.stringify(options));
-};
-
-function rext_function(name) {
-  return function (options, callback) {
-    callback(null, prefix + name + " " + JSON.stringify(options));
-  }
-}
-
-var rext = {
-  list: rext_function("LIST"),
-  create: rext_function("CREATE"),
-  update: rext_function("UPDATE"),
-  destroy: rext_function("DESTROY"),
-  retrieve: rext_function("RETRIEVE"),
-};
-
-var echo = {
-  list: echo,
-  create: echo,
-  update: echo,
-  destroy: echo,
-  retrieve: echo
-};
+var rext = new Rext("./test/repo/");
 
 app.use(connect.bodyParser());
 app.use(connect(connect.static(__dirname)));
-app.use(connect.router(rext_connect.routes({"prefix": prefix,"rext": rext})));
-app.use(connect.router(rext_connect.routes({"rext":echo}))).listen(port);
+app.use(connect.router(rext_connect.routes({"rext":rext}))).listen(port);
 
 console.log('Server running at http://127.0.0.1:' + port + '/');
 
